20927ca9923554017e6b8d5c70babf4d15e14415
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-03-31 10:37:52 +0200

    Completed

---
+++ start.js
@@ -0,0 +1,13 @@
+const apples = 20
+const oranges = 30
+const mangoes = 5
+console.log(`Are Apples and Oranges the same? ${apples === oranges}`);
+console.log(`Are Apples and Oranges not the same? ${apples !== oranges}`);
+console.log(`Are Apples greater than Oranges? ${apples > oranges}`);
+console.log(`Are Apples less than or equal to Oranges? ${apples <= oranges}`);
+console.log(`Are Oranges greater than Apples? ${oranges > apples}`);
+console.log(`Are Mangoes multiplied by Apples greater than Mangoes added to Oranges? ${mangoes * apples > mangoes + oranges}`);
+console.log(`Are Apples minus Mangoes less than Oranges divided by Mangoes? ${apples - mangoes < oranges / mangoes}`);
+console.log(`Are Mangoes, Apples and Oranges all equal? ${mangoes === apples === oranges}`);
+console.log(`Are Apples divided by Mangoes equal to Oranges divided by Mangoes? ${apples / mangoes === oranges / mangoes}`);
+console.log(`Are Mangoes plus Apples equal to Oranges minus Mangoes? ${mangoes + apples === oranges - mangoes}`);
